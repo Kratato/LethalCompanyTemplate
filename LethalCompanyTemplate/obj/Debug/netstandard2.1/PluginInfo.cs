cfefa261baed0f351bebd0a0303a1eb258dddc8a
Kratato <115898601+Kratato@users.noreply.github.com>
2023-12-21 00:02:59 +0200

    it built or smt idk

---
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalCompanyTemplate
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LC Learning";
+        public const string PLUGIN_NAME = "LC Learning";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
