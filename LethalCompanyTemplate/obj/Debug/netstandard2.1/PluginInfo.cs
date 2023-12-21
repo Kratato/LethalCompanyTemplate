d2dd904e326afef123563b77cebe3d7763407624
Kratato <115898601+Kratato@users.noreply.github.com>
2023-12-21 21:46:55 +0200

    removed obj
    it should be ignored by .gitignore but it keeps doing stuff

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LethalCompanyTemplate
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "LC Learning";
-        public const string PLUGIN_NAME = "LC Learning";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
